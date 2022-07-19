b75c68b826e32c495a60f53fd2fe6bec27a19cf4
Tony Kao <tony@progressivetech.net>
2022-07-19 01:40:27 -0400

    dhrystone w/ mpu, cache & fpu init

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
-test:
-	arm-none-eabi-gcc -mcpu=cortex-r5 -Og --specs=nosys.specs --specs=nano.specs -T gcc.ld -o test.elf test.c
+src = $(wildcard r5/*.S) \
+      $(wildcard r5/*.c)
+cflags = -fno-exceptions -mcpu=cortex-r5 -marm -mfloat-abi=hard -O3 -specs=nosys.specs -I r5 -T gcc.ld
+
+bbai64_ip = 192.168.0.122
+skam64x_ip = 192.168.0.151
+
+test.elf: $(src)
+	arm-none-eabi-gcc $(cflags) -o test.elf $(src) test.c dhry_1.c dhry_2.c
 	arm-none-eabi-size test.elf
 	arm-none-eabi-objdump -xd test.elf > test.lst
 	# sudo cp test.elf /lib/firmware/
@@ -7,3 +14,17 @@ test:
 	# sudo echo test.elf > /sys/class/remoteproc/remoteproc18/firmware
 	# sudo echo start > /sys/class/remoteproc/remoteproc18/state
 	# sudo cat /sys/kernel/debug/remoteproc/remoteproc18/trace0
+
+bbai64: test.elf
+	scp test.elf debian@$(bbai64_ip):/lib/firmware
+	ssh -t debian@$(bbai64_ip) 'sudo echo stop > /sys/class/remoteproc/remoteproc18/state ; \
+		sudo echo test.elf > /sys/class/remoteproc/remoteproc18/firmware ; \
+		sudo echo start > /sys/class/remoteproc/remoteproc18/state && \
+		sudo cat /sys/kernel/debug/remoteproc/remoteproc18/trace0'
+
+skam64x: test.elf
+	scp test.elf root@$(skam64x_ip):/lib/firmware
+	ssh root@$(skam64x_ip) 'echo stop > /sys/class/remoteproc/remoteproc2/state ; \
+		echo test.elf > /sys/class/remoteproc/remoteproc2/firmware ; \
+		echo start > /sys/class/remoteproc/remoteproc2/state && \
+		cat /sys/kernel/debug/remoteproc/remoteproc2/trace0'
--- test.c
+++ test.c
@@ -1,72 +1,167 @@
 #include <stdint.h>
 #include "ipc_rpmsg_linux_resource_table.h"
 #include <string.h>
+#include <stdio.h>
+#include "r5/kernel/dpl/CacheP.h"
+#include "r5/kernel/dpl/MpuP_armv7.h"
+#include <stdlib.h>
 
-void Reset_Handler  (void) __attribute__ ((naked));
-void Default_Handler(void);
+#include "r5/kernel/dpl/CycleCounterP.h"
 
-void Undef_Handler (void) __attribute__ ((weak, alias("Default_Handler")));
-void SVC_Handler   (void) __attribute__ ((weak, alias("Default_Handler")));
-void PAbt_Handler  (void) __attribute__ ((weak, alias("Default_Handler")));
-void DAbt_Handler  (void) __attribute__ ((weak, alias("Default_Handler")));
-void IRQ_Handler   (void) __attribute__ ((weak, alias("Default_Handler")));
-void FIQ_Handler   (void) __attribute__ ((weak, alias("Default_Handler")));
-
-__attribute__((naked, section(".isr_vector"))) void vectors() {
-  asm volatile(
-  "LDR    PC, =Reset_Handler                        \n"
-  "LDR    PC, =Undef_Handler                        \n"
-  "LDR    PC, =SVC_Handler                          \n"
-  "LDR    PC, =PAbt_Handler                         \n"
-  "LDR    PC, =DAbt_Handler                         \n"
-  "NOP                                              \n"
-  "LDR    PC, =IRQ_Handler                          \n"
-  "LDR    PC, =FIQ_Handler                          \n"
-  );
-}
-
-extern void _start();
-
-void Reset_Handler() {
-    _start();
-}
-void Default_Handler() {
-    while(1);
-}
-
-#define DebugP_MEM_LOG_SIZE 1024
-__attribute__((section (".log_shared_mem"))) char gDebugMemLog[DebugP_MEM_LOG_SIZE];
-
-const RPMessage_ResourceTable gRPMessage_linuxResourceTable __attribute__ ((section (".resource_table"), aligned (4096))) =
-{
+// global structures used by MPU and cache init code
+CacheP_Config gCacheConfig = {1, 0}; // cache on, no forced writethrough
+MpuP_Config gMpuConfig = {3, 1, 1}; // 3 regions, background region on, MPU on
+MpuP_RegionConfig gMpuRegionConfig[3] = {
+    // MSRAM region
     {
-        1U,         /* we're the first version that implements this */
-        2U,         /* number of entries, MUST be 2 */
-        { 0U, 0U, } /* reserved, must be zero */
+        .baseAddr = 0x70000000u,
+        .size = MpuP_RegionSize_2M,
+        .attrs = {
+            .isEnable = 1,
+            .isCacheable = 1,
+            .isBufferable = 1,
+            .isSharable = 0,
+            .isExecuteNever = 0,
+            .tex = 7,
+            .accessPerm = MpuP_AP_ALL_RW,
+            .subregionDisableMask = 0x0u},
     },
-    /* offsets to the entries */
+    // DDR region
     {
-        offsetof(RPMessage_ResourceTable, vdev),
-        offsetof(RPMessage_ResourceTable, trace),
+        .baseAddr = 0x80000000u,
+        .size = MpuP_RegionSize_2G,
+        .attrs = {.isEnable = 1, .isCacheable = 1, .isBufferable = 1, .isSharable = 0, .isExecuteNever = 0, .tex = 7, .accessPerm = MpuP_AP_ALL_RW, .subregionDisableMask = 0x0u},
     },
-    /* vdev entry */
+    // rpmsg region
     {
-        RPMESSAGE_RSC_TYPE_VDEV, RPMESSAGE_RSC_VIRTIO_ID_RPMSG,
-        0U, 1U, 0U, 0U, 0U, 2U, { 0U, 0U },
-    },
-    /* the two vrings */
-    { RPMESSAGE_RSC_VRING_ADDR_ANY, 4096U, 256U, 1U, 0U },
-    { RPMESSAGE_RSC_VRING_ADDR_ANY, 4096U, 256U, 2U, 0U },
-    {
-        (RPMESSAGE_RSC_TRACE_INTS_VER0 | RPMESSAGE_RSC_TYPE_TRACE),
-        (uint32_t)gDebugMemLog, DebugP_MEM_LOG_SIZE,
-        0, "trace:r5fss0_0",
+        .baseAddr = 0xA1000000u,
+        .size = MpuP_RegionSize_1M,
+        .attrs = {.isEnable = 1, .isCacheable = 0, .isBufferable = 0, .isSharable = 1, .isExecuteNever = 1, .tex = 1, .accessPerm = MpuP_AP_ALL_RW, .subregionDisableMask = 0x0u},
     },
 };
 
-const char* msg = "Hello world!\n";
+// NOTE: R5FSS implmentation defaults to ARM at reset so these must all be ARM instead of Thumb
 
-int main() {
-    strcpy(gDebugMemLog, msg);
+void Reset_Handler(void) __attribute__((naked, section(".boot.reset"), target("arm")));
+void Default_Handler(void) __attribute__((naked, section(".boot.handler"), target("arm")));
+
+void Undef_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void SVC_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void PAbt_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void DAbt_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void IRQ_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void FIQ_Handler(void) __attribute__((weak, alias("Default_Handler")));
+
+__attribute__((naked, section(".vector"), target("arm"))) void vectors()
+{
+    asm volatile(
+        "LDR    PC, =Reset_Handler                        \n"
+        "LDR    PC, =Undef_Handler                        \n"
+        "LDR    PC, =SVC_Handler                          \n"
+        "LDR    PC, =PAbt_Handler                         \n"
+        "LDR    PC, =DAbt_Handler                         \n"
+        "NOP                                              \n"
+        "LDR    PC, =IRQ_Handler                          \n"
+        "LDR    PC, =FIQ_Handler                          \n");
+}
+
+// newlib startup code
+extern void _start();
+
+void Reset_Handler()
+{
+    asm volatile(
+        // initialize stack
+        "LDR SP, =__stack \n"
+
+        // disable interrupts
+        "mrs r0, cpsr \n"
+        "orr r0, r0, #0xc0 \n"
+        "msr cpsr_cf, r0 \n"
+
+        // enable FPU
+        "mrc p15,#0x0,r0,c1,c0,#2 \n"
+        "mov r3,#0xf00000 \n"
+        "orr r0,r0,r3 \n"
+        "mcr p15,#0x0,r0,c1,c0,#2 \n"
+        "mov r0,#0x40000000 \n"
+        "fmxr fpexc,r0 \n");
+
+    // must initialize MPU if code is on external memory
+    MpuP_init();
+    CacheP_init();
+
+    _start();
+}
+
+void Default_Handler()
+{
+    while (1)
+        ;
+}
+
+__attribute__((section(".log_shared_mem"))) char gDebugMemLog[DebugP_MEM_LOG_SIZE];
+
+RPMessage_ResourceTable gRPMessage_linuxResourceTable __attribute__((section(".resource_table"), aligned(4096))) =
+    {
+        {
+            1U, /* we're the first version that implements this */
+            2U, /* number of entries, MUST be 2 */
+            {
+                0U,
+                0U,
+            } /* reserved, must be zero */
+        },
+        /* offsets to the entries */
+        {
+            offsetof(RPMessage_ResourceTable, vdev),
+            offsetof(RPMessage_ResourceTable, trace),
+        },
+        /* vdev entry */
+        {
+            RPMESSAGE_RSC_TYPE_VDEV,
+            RPMESSAGE_RSC_VIRTIO_ID_RPMSG,
+            0U,
+            1U,
+            0U,
+            0U,
+            0U,
+            2U,
+            {0U, 0U},
+        },
+        /* the two vrings */
+        {RPMESSAGE_RSC_VRING_ADDR_ANY, 4096U, 256U, 1U, 0U},
+        {RPMESSAGE_RSC_VRING_ADDR_ANY, 4096U, 256U, 2U, 0U},
+        {
+            (RPMESSAGE_RSC_TRACE_INTS_VER0 | RPMESSAGE_RSC_TYPE_TRACE),
+            (uint32_t)gDebugMemLog,
+            DebugP_MEM_LOG_SIZE,
+            0,
+            "trace:r5fss_test",
+        },
+};
+
+// retarget stdout to remoteproc trace buffer
+int _write(int handle, char *data, int size)
+{
+    static int idx = 0;
+    int count;
+
+    for (count = 0; count < size; count++)
+    {
+        if (idx > DebugP_MEM_LOG_SIZE)
+            idx = 0;
+
+        gDebugMemLog[idx++] = data[count];
+    }
+    CacheP_wb(gDebugMemLog, DebugP_MEM_LOG_SIZE, CacheP_TYPE_L1D);
+
+    return count;
+}
+
+int main()
+{
+    CycleCounterP_reset();
+    runDhrystone();
     return 0;
 }
